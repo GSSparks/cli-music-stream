1bb6ced7f48b4e8a9707c2a6d7f57aac4eaf140d
Gary Sparks <garypriscillasparks@gmail.com>
2023-10-15 11:47:01 -0400

    feat: Add the ability to add, remove, update streams

--- music_stream.sh
+++ music_stream.sh
@@ -6,21 +6,115 @@ if ! command -v mpv &> /dev/null; then
   exit 1
 fi
 
-# Define a list of stream identifiers and their corresponding URLs
+# JSON file path in the user's home directory
+JSON_FILE="$HOME/.config/streams.json"
+
+# Function to check and initialize the JSON file if it doesn't exist
+initialize_json_file() {
+  if [ ! -f "$JSON_FILE" ]; then
+    echo "JSON file does not exist. Creating one in $JSON_FILE..."
+    jsonfile='{
+      "streams": {
+        "951": "https://ice64.securenetsystems.net/WAJI?playSessionID=8882F088-FBC7-F008-2C64ACB2C771047E",
+        "1039": "https://prod-54-90-118-66.amperwave.net/adamsradio-wwfwfmaac-ibc1?",
+        "80s80s": "http://streams.80s80s.de/web/mp3-192/streams.80s80s.de/",
+        "90s90s": "http://streams.90s90s.de/grunge/mp3-192/streams.90s90s.de/",
+        "bluegrass": "https://ice24.securenetsystems.net/WAMU",
+        "country": "http://185.33.21.112/ccountry_mobile_mp3",
+        "numetal": "http://stream.revma.ihrhls.com/zc9483",
+        "oldies": "http://46.105.122.141:9676/;",
+        "vinyl": "https://icecast.walmradio.com:8443/classic"
+      },
+      "lastplayed": "951"
+    }'
+    # Create a sample JSON file with an empty object {}
+    echo $jsonfile > "$JSON_FILE"
+  fi
+}
+
+# Call the function to initialize the JSON file
+initialize_json_file
+
+# Function to read the stream URLs from the JSON file and populate the 'streams' associative array
+
 declare -A streams
-streams["WAJI"]="https://ice64.securenetsystems.net/WAJI?playSessionID=8882F088-FBC7-F008-2C64ACB2C771047E"
-streams["Wayne"]="https://prod-54-90-118-66.amperwave.net/adamsradio-wwfwfmaac-ibc1?"
-streams["80s80s"]="http://streams.80s80s.de/web/mp3-192/streams.80s80s.de/"
-streams["90s90s"]="http://streams.90s90s.de/grunge/mp3-192/streams.90s90s.de/"
-streams["Bluegrass"]="https://ice24.securenetsystems.net/WAMU"
-streams["Country"]="http://185.33.21.112/ccountry_mobile_mp3"
-streams["Numetal"]="http://stream.revma.ihrhls.com/zc9483"
-streams["Oldies"]="http://46.105.122.141:9676/;"
-streams["Vinyl"]="https://icecast.walmradio.com:8443/classic"
+
+# Use jq to extract the "streams" object from the JSON file and format it as a JSON string
+streams_json=$(jq '.streams | to_entries[] | "\(.key) \(.value)"' "$JSON_FILE")
+
+# Iterate over the key-value pairs and populate the streams array
+while IFS=' ' read -r key value; do
+  key=$(echo "$key" | tr -d '"')
+  value=$(echo "$value" | tr -d '"')
+  streams["$key"]="$value"
+done <<< "$streams_json"
+
+# Function to add a new stream
+add_stream() {
+  local key="$1"
+  local url="$2"
+
+  if [ -z "$key" ] || [ -z "$url" ]; then
+    echo "Usage: $0 add <key> <url>"
+    exit 1
+  fi
+
+  if [ -n "${streams["$key"]}" ]; then
+    echo "Stream with key '$key' already exists. Use 'update' to change it."
+    exit 1
+  fi
+  tmpJson=$(cat $JSON_FILE)
+  jq --arg key "$key" --arg url "$url" '.streams[$key] = $url' <<< "$tmpJson" > /tmp/music_stream_tmp.json
+  mv /tmp/music_stream_tmp.json "$JSON_FILE"
+  echo "Stream added: $key -> $url"
+}
+
+# Function to remove an existing stream
+remove_stream() {
+  local key="$1"
+
+  if [ -z "$key" ]; then
+    echo "Usage: $0 remove <key> "
+    exit 1
+  fi
+
+  if [ -z "${streams["$key"]}" ]; then
+    echo "Stream with key '$key' does not exist."
+    exit 1
+  fi
+
+  tmpJson=$(cat $JSON_FILE)
+  jq --arg key "$key" 'del(.streams[$key])' <<< "$tmpJson" > /tmp/music_stream_tmp.json
+  mv /tmp/music_stream_tmp.json "$JSON_FILE"
+
+  echo "Stream $key has been removed"
+}
+
+# Function to update an existing stream
+update_stream() {
+  local key="$1"
+  local url="$2"
+
+  if [ -z "$key" ] || [ -z "$url" ]; then
+    echo "Usage: $0 update <key> <url>"
+    exit 1
+  fi
+
+  if [ -z "${streams["$key"]}" ]; then
+    echo "Stream with key '$key' does not exist. Use 'add' to create it."
+    exit 1
+  fi
+
+  tmpJson=$(cat $JSON_FILE)
+  jq --arg key "$key" --arg url "$url" '.streams[$key] = $url' <<< "$tmpJson" > /tmp/music_stream_tmp.json
+  mv /tmp/music_stream_tmp.json "$JSON_FILE"
+
+  echo "Stream updated: $key -> $url"
+}
 
 # Define variables
 caller="$0"
-last_selected_stream="$(cat ~/.music_stream)"
+last_selected_stream="$(jq -r '.lastplayed' $JSON_FILE)"
 
 is_playing() {  # function to keep next and prev from starting a stream
   playing=$(pgrep -a "mpv" | grep "$urls" | awk '{print $3}')
@@ -47,7 +141,7 @@ start_stream() {
     exit
   fi
 
-  # This section will stop any running streams in the event we're "chang" stations
+  # This section will stop any running streams in the event we're "changing" stations
   playing=$(pgrep -a "mpv" | grep "$urls" | awk '{print $3}')
   if [ -n "$playing" ]; then
     stop_stream
@@ -56,7 +150,13 @@ start_stream() {
   local selected_stream="${streams[$1]}"
   if [ -n "$selected_stream" ]; then
     mpv "$selected_stream" > /dev/null 2>&1 &
-    echo "$1" > ~/.music_stream
+
+    # This section updates the lastplayed value
+    tmpJson=$(cat $JSON_FILE)
+    streaming=$1
+    jq '.lastplayed = $streaming' --arg streaming $streaming <<<"$tmpJson" > /tmp/music_stream_tmp.json
+    mv /tmp/music_stream_tmp.json "$JSON_FILE"
+
     echo "Streaming $1..."
     notify-send -a "Music Streaming" "$1"
   else
@@ -117,8 +217,14 @@ prev_stream() {
 }
 
 usage() {
+  echo "CLI Music Streamer: This script plays music streams in the background"
+  echo "from the command line using mpv music player."
   echo ""
-  echo "Usage: $(basename $0) [play <stream>] [stop]"
+  echo "Usage: $(basename $0) [play <stream>] [stop] [next] [prev]"
+  echo ""
+  echo "       Add a stream:    [add <stream> <url>]"
+  echo "       Update a stream: [update <stream> <url>]"
+  echo "       Remove a stream: [remove <stream>]"
   echo "----------------------------------------------------------"
   echo "The available streams are:"
   echo ""
@@ -135,6 +241,12 @@ elif [ "$1" == "next" ]; then
   next_stream "$last_selected_stream"
 elif [ "$1" == "prev" ]; then
   prev_stream "$last_selected_stream"
+elif [ "$1" == "add" ]; then
+  add_stream "$2" "$3"
+elif [ "$1" == "update" ]; then
+  update_stream "$2" "$3"
+elif [ "$1" == "remove" ]; then
+  remove_stream "$2"
 else
   usage
 fi
